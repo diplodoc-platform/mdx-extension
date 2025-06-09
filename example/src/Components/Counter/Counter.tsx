5409c24ef24ccca8353cbfd20e0b002f03c6d9ec
Anton Vikulov <rndnm@yandex-team.ru>
2025-06-09 14:05:56 +0300

    add use state ctx

--- example/src/Components/Counter/Counter.tsx
+++ example/src/Components/Counter/Counter.tsx
@@ -1,12 +1,20 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Button} from '@gravity-ui/uikit';
+import {MdxSetStateCtx, type MdxSetStateCtxValue, MdxStateCtx} from '@plugin';
 
 type CounterProps = {
     initialValue?: number;
 };
 
 export const Counter: React.FC<CounterProps> = ({initialValue = 0}) => {
-    const [count, setCount] = useState(initialValue);
+    const mdxState = useContext(MdxStateCtx as React.Context<CounterProps>);
+    const setMdxState = useContext(MdxSetStateCtx as React.Context<MdxSetStateCtxValue>);
+
+    if (setMdxState) {
+        setMdxState({initialValue});
+    }
+
+    const [count, setCount] = useState(mdxState.initialValue ?? initialValue);
 
     return (
         <div style={{display: 'flex', alignItems: 'center'}}>

fc293493b5eb4b2cc9d20db94a230c03f9375ec6
Anton <Feverqwe@users.noreply.github.com>
2025-06-24 17:52:51 +0300

    feat(build): add commonjs (#23)

---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import type {Configuration} from 'webpack';
+import * as path from 'node:path';
+
+const config: Configuration = {
+    entry: './src/index.ts',
+    mode: 'production',
+    optimization: {
+        minimize: false,
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: `index.js`,
+        path: path.resolve('./build/cjs'),
+        libraryTarget: 'commonjs2',
+    },
+    externals: ['react', 'react-dom'],
+};
+
+export default config;

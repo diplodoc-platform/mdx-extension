d1adfbc3b05d98335385f78b5d76d9262800ce76
Anton <Feverqwe@users.noreply.github.com>
2025-06-25 12:38:47 +0300

    fix(cjs): exclude jsx-runtime (#25)

--- webpack.config.ts
+++ webpack.config.ts
@@ -25,7 +25,7 @@ const config: Configuration = {
         path: path.resolve('./build/cjs'),
         libraryTarget: 'commonjs2',
     },
-    externals: ['react', 'react-dom'],
+    externals: ['react', 'react-dom', 'react/jsx-runtime'],
 };
 
 export default config;

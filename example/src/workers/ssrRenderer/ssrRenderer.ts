caacd36f789f9c1b0ad15f71046b5ceba90e5509
Anton <Feverqwe@users.noreply.github.com>
2025-07-02 12:04:32 +0300

    chore(example): add babel env and terser (#35)

--- example/src/workers/ssrRenderer/ssrRenderer.ts
+++ example/src/workers/ssrRenderer/ssrRenderer.ts
@@ -7,6 +7,8 @@ import {isWithMdxArtifacts, mdxPlugin} from '@plugin';
 import {PURE_COMPONENTS, SSR_COMPONENTS} from '@/components';
 import getAsyncSsrRenderer from '../../../../src/utils/getAsyncSsrRenderer';
 import assert from 'node:assert';
+import {minify} from 'terser';
+import * as babel from '@babel/core';
 
 export interface SsrRendererWorker {
     getContent: typeof getContent;
@@ -30,6 +32,38 @@ const getContent = async (content: string) => {
 
     assert(mdxArtifacts);
     const htmlWithMdx = await renderAsync(html, mdxArtifacts);
+
+    const keys = Object.keys(mdxArtifacts.idMdx);
+
+    for (let i = 0, id; (id = keys[i]); i++) {
+        const code = mdxArtifacts.idMdx[id];
+
+        const parsedAst = await babel.parseAsync(code, {
+            parserOpts: {allowReturnOutsideFunction: true},
+        });
+        assert(parsedAst);
+
+        const transformedResult = await babel.transformFromAstAsync(parsedAst, code, {
+            presets: ['@babel/preset-env'],
+            ast: false,
+            code: true,
+        });
+        assert(transformedResult?.code);
+
+        const {code: miniCode = ''} = await minify(transformedResult.code, {
+            mangle: {
+                module: true,
+            },
+            compress: {
+                module: true,
+            },
+            parse: {
+                bare_returns: true,
+            },
+        });
+
+        mdxArtifacts.idMdx[id] = miniCode;
+    }
 
     return {html: htmlWithMdx, mdxArtifacts};
 };

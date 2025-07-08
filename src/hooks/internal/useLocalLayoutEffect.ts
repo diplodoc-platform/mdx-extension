72ea6ef26a1631e713aba5aeb3d8a9977ab9ee8b
Anton Vikulov <rndnm@yandex-team.ru>
2025-07-08 07:21:59 +0300

    fix lint

--- src/hooks/internal/useLocalLayoutEffect.ts
+++ src/hooks/internal/useLocalLayoutEffect.ts
@@ -4,4 +4,4 @@ import {useEffect, useLayoutEffect} from 'react';
  * Just mute errors on server with useLayoutEffect
  */
 
-export const useLocalLayoutEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect;
+export const useLocalLayoutEffect = typeof window === 'undefined' ? useEffect : useLayoutEffect;
